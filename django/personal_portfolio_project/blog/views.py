9e5278ca1a7392dc8a8d38d72fe46c98e27d96e9
Leo <leo.80@protonmail.com>
2020-05-23 23:21:45 -0300

    material django: static files e blog id's

--- django/personal_portfolio_project/blog/views.py
+++ django/personal_portfolio_project/blog/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Posts
 
 def all_blogs(request):
   posts = Posts.objects.order_by('-date')[:5]
   return render(request, 'blog/all_blogs.html', {'posts': posts})
+
+def detail(request, blog_id):
+  blog = get_object_or_404(Posts, pk=blog_id)
+  return render(request, 'blog/detail.html', {'id': blog})
